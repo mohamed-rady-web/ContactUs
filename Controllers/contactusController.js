9dcc796277568ec6bd938618794ad6b0de88f50b
Mohamed Rady <mohamed.rady99990@gmail.com>
2025-05-02 15:32:48 +0200

    4th commit

--- Controllers/contactusController.js
+++ Controllers/contactusController.js
@@ -7,7 +7,7 @@ exports.message = async (req, res) => {
         const newmessage = new messageModel({ name, email, service, projectDetails });
         newmessage.save();
         res.status(200).json({ message: 'Message sent successfully' });
-         sendEmail(email, "Contact Us", `Name: ${name}\nEmail: ${email}\nPhone Number: ${service}\nMessage: ${projectDetails}`);
+         sendEmail(email, "Contact Us", `Name: ${name}\nEmail: ${email}\nservice: ${service}\nprojectDetails: ${projectDetails}`);
     } catch (error) {
         console.error("Error saving message information:", error);
         res.status(500).json({ message: "Something went wrong" });
